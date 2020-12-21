c2e9ba37253d3a0656ccdb4db29875370d87fdf0
Jonathan Knorn <j.knorn@gmail.com>
2020-12-21 19:32:16 +0100

    Add about text and loaction functionality

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,16 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const button = document.querySelector('button')
 
+weatherForm.addEventListener('input', (e) => {
+  const text = search.value
+  if(text.length == 0){
+    button.innerHTML = 'My location'
+  } else {
+    button.innerHTML = 'Search'
+  }
+})
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -14,6 +23,22 @@ weatherForm.addEventListener('submit', (e) => {
   messageOne.textContent = 'Loading...'
   messageTwo.textContent = ''
 
+  if(location.length == 0){
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(fetchWeather);
+    } else {
+      fetchWather(undefined, '')
+    }
+  } else {
+    fetchWeather(undefined, location)
+  }
+})
+
+const fetchWeather = (position, location) => {
+  if(position){
+    location = position.coords.longitude + ',' + position.coords.latitude
+  }
+  console.log(location)
   fetch('/weather?address=' + location).then((response) => {
     response.json().then(({error, forecast, location, address} = {}) => {
       if(error){
@@ -24,5 +49,4 @@ weatherForm.addEventListener('submit', (e) => {
       }
     })
   })
-
-})
+}
